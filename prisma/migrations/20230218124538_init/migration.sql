6b2f264fd3b803dc0b03f2050b230cdd546e2ac7
Jose Javier Sanahuja <josejaviersanahuja@gmail.com>
2023-02-18 13:35:51 +0000

    change db motor to mysql to avoid bug pg

---
+++ prisma/migrations/20230218124538_init/migration.sql
@@ -0,0 +1,108 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `hashedPassword` VARCHAR(191) NOT NULL,
+    `role` ENUM('USER', 'WRITER', 'ADMIN', 'SUPER_ADMIN') NOT NULL DEFAULT 'USER',
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Profile` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `bio` VARCHAR(191) NOT NULL,
+    `displayName` VARCHAR(191) NOT NULL,
+    `familyName` VARCHAR(191) NULL,
+    `givenName` VARCHAR(191) NULL,
+    `middleName` VARCHAR(191) NULL,
+    `photoURL` VARCHAR(191) NULL,
+    `userId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Profile_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `status` ENUM('DRAFT', 'PUBLISHED', 'DELETED') NOT NULL DEFAULT 'DRAFT',
+    `titleSpanish` VARCHAR(191) NOT NULL,
+    `titleEnglish` VARCHAR(191) NOT NULL,
+    `contentSpanish` VARCHAR(191) NULL,
+    `contentEnglish` VARCHAR(191) NULL,
+    `authorId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Comment` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `postId` VARCHAR(191) NOT NULL,
+    `parentId` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Like_Post` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `postId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Like_Post_userId_postId_key`(`userId`, `postId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Like_Comment` (
+    `id` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `commentId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Like_Comment_userId_commentId_key`(`userId`, `commentId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Profile` ADD CONSTRAINT `Profile_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `Post`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_parentId_fkey` FOREIGN KEY (`parentId`) REFERENCES `Comment`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Like_Post` ADD CONSTRAINT `Like_Post_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Like_Post` ADD CONSTRAINT `Like_Post_postId_fkey` FOREIGN KEY (`postId`) REFERENCES `Post`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Like_Comment` ADD CONSTRAINT `Like_Comment_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Like_Comment` ADD CONSTRAINT `Like_Comment_commentId_fkey` FOREIGN KEY (`commentId`) REFERENCES `Comment`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,59 +3,51 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function seed () {
-  // await prisma.post.deleteMany()
-  // await prisma.user.deleteMany()
-  console.log('Seeding the database...')
+  await prisma.post.deleteMany()
+  await prisma.user.deleteMany()
+
   await prisma.user.create({
     data: {
       email: 'zitrojj@gmail.com',
       hashedPassword: '123456'
     }
   })
-  console.log('Seeding the database...')
-  // await prisma.user.create({
-  //   data: {
-  //     email: 'azjj0610@gmail.com',
-  //     hashedPassword: '123456'
-  //   }
-  // })
-  // const post = await prisma.post.create({
-  //   data: {
-  //     titleSpanish: 'Hola Mundo',
-  //     titleEnglish: 'Hello World',
-  //     contentSpanish: 'Este es mi primer post',
-  //     contentEnglish: 'This is my first post',
-  //     status: 'PUBLISHED',
-  //     author: {
-  //       connect: {
-  //         email: 'zitrojj@gmail.com'
-  //       }
-  //     }
-  //   }
-  // })
-  // await prisma.comment.create({
-  //   data: {
-  //     content: 'This is my first comment',
-  //     user: {
-  //       connect: {
-  //         email: 'azjj0610@gmail.com'
-  //       }
-  //     },
-  //     post: {
-  //       connect: {
-  //         id: post.id
-  //       }
-  //     }
-  //   }
-  // })
+
+  await prisma.user.create({
+    data: {
+      email: 'azjj0610@gmail.com',
+      hashedPassword: '123456'
+    }
+  })
+  const post = await prisma.post.create({
+    data: {
+      titleSpanish: 'Hola Mundo',
+      titleEnglish: 'Hello World',
+      contentSpanish: 'Este es mi primer post',
+      contentEnglish: 'This is my first post',
+      status: 'PUBLISHED',
+      author: {
+        connect: {
+          email: 'zitrojj@gmail.com'
+        }
+      }
+    }
+  })
+  await prisma.comment.create({
+    data: {
+      content: 'This is my first comment',
+      user: {
+        connect: {
+          email: 'azjj0610@gmail.com'
+        }
+      },
+      post: {
+        connect: {
+          id: post.id
+        }
+      }
+    }
+  })
 }
 
 seed()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
